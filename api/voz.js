b4115e101f4855b2305063c484e3664856efa97d
4ngelux <72478476+4ngelux@users.noreply.github.com>
2022-01-29 19:19:02 -0500

    Update

---
+++ api/voz.js
@@ -0,0 +1,20 @@
+const messagebird = require('messagebird')(process.env.KEY_BIRD_SMS);
+const log = console.log;
+const express = require('express');
+const app = express();
+const _ = require('lodash');
+const colors = require('colors');
+const red = colors.red;
+const yellow = colors.yellow;
+const cyan = colors.cyan;
+const green = colors.green;
+
+var voz = module.exports = {
+    sendMenssage: async function () {
+        return new Promise(
+            (resolve, reject) => {
+              
+            }
+        );
+    },
+}
